31795c0cf9486bbb411c9c2b1fd2a70927b9c4f7
yofieedr666 <yofiemadridista@gmail.com>
2023-06-17 16:40:33 +0800

    Membuat class Lingkaran Dan Persegi Panjang

--- src/geometri/PersegiPanjang.java
+++ src/geometri/PersegiPanjang.java
@@ -1,26 +1,19 @@
-
 package geometri;
 
-public class PersegiPanjang {
-    double bil1, bil2;
+public class PersegiPanjang{
+private final double panjang;
+private final double lebar;
 
-    public PersegiPanjang(double bil1, double bil2) {
-        this.bil1 = bil1;
-        this.bil2 = bil2;
+    public PersegiPanjang(double panjang, double lebar) {
+        this.panjang = panjang;
+        this.lebar = lebar;
     }
-    
-    double setLuas(){
-       return bil1 * bil2;
-   }
-     double setKeliling(){
-       return 2*(bil1+bil2);
-   }
-     
-     public static void main(String[] args) {
-        PersegiPanjang luas = new PersegiPanjang(7,1);
-        PersegiPanjang keliling = new PersegiPanjang(7,1);
-        
-         System.out.println("Hasil    ="+luas.setLuas());
-         System.out.println("Hasil P  ="+luas.setKeliling());
-}
+
+    public double luas() {
+        return panjang * lebar;
+    }
+
+    public double keliling() {
+        return 2 * (panjang + lebar);
+    }
 }
